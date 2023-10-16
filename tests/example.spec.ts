1c17103ae32e3553a6977ff3b6fbee029ad1f0c2
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2023-10-16 20:37:16 +0200

    Add Example UI Test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('Is Truthy', async () => {
-  expect(true).toBeTruthy();
+test.describe('Web UI Demo', async () => {
+  test('Github Homepage', async ({ page }) => {
+    await test.step('Open Github', async () => {
+      await page.goto('https://github.com/');
+    });
+
+    await test.step('Validate Homepage Header', async () => {
+      return expect(page.getByLabel('Homepage', { exact: true })).toBeVisible({ timeout: 5000 });
+    });
+  });
 });
